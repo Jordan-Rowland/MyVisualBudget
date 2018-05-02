28d834388318cb2de7c7a88d445a6e3c9c710974
Jordan Rowland <36084892+Jordan-Rowland@users.noreply.github.com>
2018-05-02 14:47:49 -0700

    Rewrote lots of the JS, added functions to store in LocalStorage

--- script.js
+++ script.js
@@ -1,139 +1,179 @@
+class Expense {
+	constructor(name, cost, percentage) {
+		this.name = name;
+		this.cost = cost;
+		this.percentage = percentage;
+	}
+}
+
+class Store {
+
+  static getExpenses() {
+    let expenses;
+    if (localStorage.getItem('expenses') === null) {
+      expenses = [];
+    } else {
+      expenses = JSON.parse(localStorage.getItem('expenses'));
+    }
+    // console.log(budget.expenses);
+
+    return expenses;
+  }
+
+  static displayExpenses() {
+  	budget.expenses = []
+    const expenses = Store.getExpenses();
+    expenses.forEach(function (expense, i, arr) {
+      expense = new Expense;
+      budget.expenses.push(expenses[i]);
+    // console.log(budget.expenses);
+
+      return;
+
+    })
+  }
+
+  static addExpense(expense) {
+    const expenses = Store.getExpenses();
+    expenses.push(expense);
+    localStorage.setItem('expenses', JSON.stringify(expenses));
+    // console.log(budget.expenses);
+
+  }
+
+  static removeExpense(name) {
+    const expenses = Store.getExpenses();
+    expenses.forEach(function (expense, index) {
+      if (expense.name === name) {
+        expenses.splice(index, 1);
+      }
+    });
+    localStorage.setItem('expenses', JSON.stringify(expenses));
+    Store.displayExpenses
+    // console.log(budget.expenses);
+
+  }
+
+}
+
+
+
+
+let check = document.querySelector('#check');
+let monthly = document.querySelector('#monthly');
+let biWeekly = document.querySelector('#bi-weekly');
+let weekly = document.querySelector('#weekly');
+let expenseName = document.querySelector('#expenseName');
+let expenseCost = document.querySelector('#expenseCost');
+let percentage = document.querySelector('#percentage')
+let add = document.querySelector('#add');	
+let remove = document.querySelector('#remove');	
+
+
 let budget = {
-	expenses: {},
+	expenses: [],
 
 	addEditExpense: function() {
-		let check = document.querySelector('#check');
-		let expenseName = document.querySelector('#expenseName');
-		let expenseCost = document.querySelector('#expenseCost');
+		let expense;
 
-		let monthly = document.querySelector('#monthly');
-		let biWeekly = document.querySelector('#bi-weekly');
-		let weekly = document.querySelector('#weekly');
+    if (percentage.checked) {
+    	expense = new Expense(expenseName.value, Number(expenseCost.value), true)
+    }else{
+    	expense = new Expense(expenseName.value, Number(expenseCost.value), false)
+    }
+    expenseName.value = ''
+    expenseCost.value = ''
+    budget.expenses.push(expense);
+    Store.addExpense(expense)
+    // console.log(budget.expenses);
+    view.displayBudget();
+    percentage.checked = false;
+		
+     },
 
-		if (expenseCost.value.startsWith('.')) {
-      this.expenses[expenseName.value] = Number(check.value * expenseCost.value);
-		} else {
-			this.expenses[expenseName.value] = Number(expenseCost.value);
-		}
-	},
-
-	deleteExpense: function() {
-		let expenseName = document.querySelector('#expenseName');
-		delete this.expenses[expenseName.value];
-
-		console.log(this.expenses);
-	},
-};
-
-let handlers = {
-	addEditExpense: function() {
-		let addExpenseInput = document.querySelector('#expenseName');
-		let addCostInput = document.querySelector('#expenseCost');
-		budget.addEditExpense(addExpenseInput.value);
-		addExpenseInput.value = '';
-		addCostInput.value = '';
-		addExpenseInput.focus();
+  deleteExpense: function() {
+    let expenseName = document.querySelector('#expenseName');
+    for (let i = 0; i < budget.expenses.length; i++) {
+   		if (expenseName.value === budget.expenses[i].name){
+   			budget.expenses.splice(i, 1);
+   			Store.removeExpense(expenseName.value)
+   		}
+    }
+    expenseName.value = '';
+    // console.log(budget.expenses);
 		view.displayBudget();
-	},
-
-	deleteExpense: function() {
-		let addExpenseInput = document.querySelector('#expenseName');
-		let addCostInput = document.querySelector('#expenseCost');
-		budget.deleteExpense();
-		addExpenseInput.value = '';
-		addCostInput.value = '';
-		addExpenseInput.focus();
-		view.displayBudget();
-	},
-	visualizeBudget: function() {
-		let check = document.querySelector('#check');
-		let addExpenseInput = document.querySelector('#expenseName');
-		let totalCost = 0;
-
-		for (let i = 0; i < Object.entries(budget.expenses).length; i++) {
-			totalCost += Number(Object.entries(budget.expenses)[i][1]);
-		}
-
-		addExpenseInput.focus();
-		let monthly = document.querySelector('#monthly');
-		let biWeekly = document.querySelector('#bi-weekly');
-		let weekly = document.querySelector('#weekly');
-		view.displayBudget();
-	},
+  },
 };
 
 view = {
-	displayBudget: function() {
-		let expensesUl = document.querySelector('ul');
-		expensesUl.innerHTML = '';
-		let addCostInput = document.querySelector('#expenseCost');
-		let check = document.querySelector('#check');
-		let totalCost = 0;
+  displayBudget: function () {
+    let expensesUl = document.querySelector('ul');
+    expensesUl.innerHTML = '';
+    let addCostInput = document.querySelector('#expenseCost');
+    let check = document.querySelector('#check');
+    let totalCost = 0;
 
-		let checkViz = document.createElement('li');
-		checkViz.textContent = `Check: ${check.value}`;
-		expensesUl.appendChild(checkViz);
+    let checkViz = document.createElement('li');
+    checkViz.textContent = `Check: ${check.value}`;
+    expensesUl.appendChild(checkViz);
 
-		Object.entries(budget.expenses).forEach(function(expense, i) {
-			let expenseLi = document.createElement('li');
+    Store.displayExpenses();
 
-			expenseLi.id = i;
+    budget.expenses.forEach(function (expense, i) {
+    	Store.displayExpenses();
+      let expenseLi = document.createElement('li');
 
-			let monthly = document.querySelector('#monthly');
-			let biWeekly = document.querySelector('#bi-weekly');
-			let weekly = document.querySelector('#weekly');
+      expenseLi.id = i;
 
-			if (biWeekly.checked) {
-				expenseLi.textContent = `${expense[0]}: ${expense[1] / 2}`;
-			} else if (weekly.checked) {
-				expenseLi.textContent = `${expense[0]}: ${expense[1] / 4}`;
-			} else {
-				expenseLi.textContent = `${expense[0]}: ${expense[1]}`;
-			}
+      if (budget.expenses[i].percentage === true) {
+        expenseLi.textContent =
+          `${budget.expenses[i].name}: ${Math.floor(Number(budget.expenses[i].cost * check.value  / 100))}`;
+        totalCost += (Math.floor(Number(
+          `${budget.expenses[i].cost * check.value / 100}`)));
+        expensesUl.appendChild(expenseLi);
 
-			expensesUl.appendChild(expenseLi);
-		}, this);
 
-		let monthly = document.querySelector('#monthly');
-		let biWeekly = document.querySelector('#bi-weekly');
-		let weekly = document.querySelector('#weekly');
+      } else {
+        if (biWeekly.checked) {
+          expenseLi.textContent =
+            `${budget.expenses[i].name}: ${Number(budget.expenses[i].cost / 2)}`;
+          totalCost += (Math.floor(Number(budget.expenses[i].cost / 2)));
 
-		for (let i = 0; i < Object.entries(budget.expenses).length; i++) {
-			if (biWeekly.checked) {
-				totalCost += Number(Object.entries(budget.expenses)[i][1] / 2);
-			} else if (weekly.checked) {
-				totalCost += Number(Object.entries(budget.expenses)[i][1] / 4);
-			} else {
-				totalCost += Number(Object.entries(budget.expenses)[i][1]);
-			}
-		}
+        } else if (weekly.checked) {
+          expenseLi.textContent =
+            `${budget.expenses[i].name}: ${Math.floor(Number(budget.expenses[i].cost / 4))}`;
+          totalCost += (Math.floor(Number(budget.expenses[i].cost / 4)));
 
-		let totalViz = document.createElement('li');
-		totalViz.textContent = `Total Cost: ${totalCost}`;
-		expensesUl.appendChild(totalViz);
-		let leftOver = document.createElement('li');
-		leftOver.textContent = `Left Over: ${check.value - totalCost}`;
-		expensesUl.appendChild(leftOver);
-	},
-};
+        } else {
+          expenseLi.textContent =
+            `${budget.expenses[i].name}: ${Math.floor(Number(budget.expenses[i].cost))}`;
+          totalCost += Math.floor(Number(budget.expenses[i].cost));
+        }
 
-let check = document.querySelector('#check');
-check.addEventListener('keypress', function(e) {
-	if (e.keyCode === 13) {
-		handlers.visualizeBudget();
-	}
-});
+        expensesUl.appendChild(expenseLi);
+      }
+    });
 
-let expenseCost = document.querySelector('#expenseCost');
-expenseCost.addEventListener('keypress', function(e) {
-	if (e.keyCode === 13) {
-		handlers.addEditExpense();
-	}
-});
+    let totalViz = document.createElement('li');
+    totalViz.textContent = `Total Cost: ${totalCost}`;
+    expensesUl.appendChild(totalViz);
+    let leftOver = document.createElement('li');
+    leftOver.textContent = `Left Over: ${check.value - totalCost}`;
+    expensesUl.appendChild(leftOver);
+	
 
-let expenseName = document.querySelector('#expenseName');
-expenseName.addEventListener('keypress', function(e) {
-	if (e.keyCode === 13) {
-		expenseCost.focus();
-	}
-});
+  }
+}
+
+view.displayBudget()
+
+document.addEventListener('DOMContentLoaded', Store.displayExpenses);
+
+add.addEventListener('click', budget.addEditExpense);
+remove.addEventListener('click', budget.deleteExpense);
+
+biWeekly.addEventListener('click', view.displayBudget)
+
+weekly.addEventListener('click', view.displayBudget)
+
+monthly.addEventListener('click', view.displayBudget)
